1afedd2217175d511c780e535eb38b45449754dc
BakhtiyorSharipov <bsharipov731@gmail.com>
2026-06-23 18:14:10 +0500

    implemented-unit-test-for-company-endpoint-handler

---
+++ QApiGateway.UnitTest/EndpointTests/CompanyEndpointTests/GetCompanyInfoByIdTests.cs
@@ -0,0 +1,107 @@
+using System.Net;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Moq.Protected;
+using QApiGateway.EndpointHandlers;
+using QApiGateway.Endpoints;
+using Shouldly;
+
+namespace QApiGateway.UnitTest.EndpointTests.CompanyEndpointTests;
+
+public class GetCompanyInfoByIdTests
+{
+    private readonly Mock<HttpMessageHandler> _mockHttpMessageHandler;
+    private readonly HttpClient _httpClient;
+    private readonly WebApplication _app;
+
+    public GetCompanyInfoByIdTests()
+    {
+        _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+        _httpClient = new HttpClient(_mockHttpMessageHandler.Object);
+
+        var builder = WebApplication.CreateBuilder();
+        builder.Services.AddSingleton(_httpClient);
+        _app = builder.Build();
+        _app.MapCompanyEndpoints();
+    }
+
+    [Fact]
+    public async Task GetCompanyInfoById_Should_Return_CompanyInfo_When_Authorized()
+    {
+        // Arrange
+        var companyId = 1;
+        var expectedResponse = "{company: id=1, name= Test Name}";
+        var token = "Bearer test-token";
+
+        _mockHttpMessageHandler
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(expectedResponse, Encoding.UTF8, "application/json")
+            });
+
+        var context = new DefaultHttpContext();
+        context.Request.Headers["Authorization"] = token;
+
+        var client = _httpClient;
+
+        // Act
+        var result = await CompanyEndpointsHandlers.GetCompanyInfoById(
+            client,
+            context.Request,
+            companyId);
+
+        // Assert
+        result.ShouldNotBeNull();
+        var contentResult = result.ShouldBeOfType<ContentHttpResult>();
+        contentResult.StatusCode.ShouldBe((int)HttpStatusCode.OK);
+        contentResult.ResponseContent.ShouldBe(expectedResponse);
+    }
+
+    [Fact]
+    public async Task GetCompanyInfoById_Should_Return_Error_When_Company_Not_Found()
+    {
+        // Arrange
+        var companyId = 1;
+        var expectedResponse = "{error: Company not found}";
+        var token = "Bearer test-token";
+
+        _mockHttpMessageHandler
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.NotFound,
+                Content = new StringContent(expectedResponse, Encoding.UTF8, "application/json")
+            });
+
+        var context = new DefaultHttpContext();
+        context.Request.Headers["Authorization"] = token;
+
+        var client = _httpClient;
+
+        // Act
+        var result = await CompanyEndpointsHandlers.GetCompanyInfoById(
+            client,
+            context.Request,
+            companyId);
+
+        // Assert
+        result.ShouldNotBeNull();
+        var contentResult = result.ShouldBeOfType<ContentHttpResult>();
+        contentResult.StatusCode.ShouldBe((int)HttpStatusCode.NotFound);
+        contentResult.ResponseContent.ShouldBe(expectedResponse);
+    }
+}
