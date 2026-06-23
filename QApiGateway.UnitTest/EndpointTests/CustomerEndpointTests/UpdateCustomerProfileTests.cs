88c4e64032ad37c5cc889ddb9792e4c038f42da7
BakhtiyorSharipov <bsharipov731@gmail.com>
2026-06-23 18:22:08 +0500

    implemented-unit-tests-for-customer-endpoint-handler

---
+++ QApiGateway.UnitTest/EndpointTests/CustomerEndpointTests/UpdateCustomerProfileTests.cs
@@ -0,0 +1,128 @@
+using System.Net;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Moq.Protected;
+using QApiGateway.DTO.CustomerDTO;
+using QApiGateway.EndpointHandlers;
+using QApiGateway.Endpoints;
+using Shouldly;
+
+namespace QApiGateway.UnitTest.EndpointTests.CustomerEndpointTests;
+
+public class UpdateCustomerProfileTests
+{
+    private readonly Mock<HttpMessageHandler> _mockHttpMessageHandler;
+    private readonly HttpClient _httpClient;
+    private readonly WebApplication _app;
+
+    public UpdateCustomerProfileTests()
+    {
+        _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+        _httpClient = new HttpClient(_mockHttpMessageHandler.Object);
+
+        var builder = WebApplication.CreateBuilder();
+        builder.Services.AddSingleton(_httpClient);
+        _app = builder.Build();
+        _app.MapCustomerEndpoints();
+    }
+
+    [Fact]
+    public async Task GetCustomerProfile_Should_Return_CustomerProfile_When_Authorized()
+    {
+        // Arrange
+        var request = new UpdateProfileRequest
+        {
+            FirstName = "Test Firstname",
+            LastName = "Test Lastname",
+            PhoneNumber = "+992923324252",
+            Country = "Test Country",
+            City = "Test City",
+            Address = "Test Address",
+            Gender = "Test Gender",
+            PostalCode = "Test Code",
+            DateOfBirth = new DateTime(2006, 06, 06)
+        };
+        var expectedResponse = "{customerProfile: id=1, FirstName = Test Name, LastName= Test Name}";
+        var token = "Bearer test-token";
+
+        _mockHttpMessageHandler
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(expectedResponse, Encoding.UTF8, "application/json")
+            });
+
+        var context = new DefaultHttpContext();
+        context.Request.Headers["Authorization"] = token;
+
+        var client = _httpClient;
+
+        // Act
+        var result = await CustomerEndpointsHandlers.UpdateCustomerProfile(
+            client,
+            context.Request, request);
+
+        // Assert
+        result.ShouldNotBeNull();
+        var contentResult = result.ShouldBeOfType<ContentHttpResult>();
+        contentResult.StatusCode.ShouldBe((int)HttpStatusCode.OK);
+        contentResult.ResponseContent.ShouldBe(expectedResponse);
+    }
+
+    [Fact]
+    public async Task GetCustomerProfile_Should_Return_Error_When_Profile_Not_Found()
+    {
+        // Arrange
+        var request = new UpdateProfileRequest
+        {
+            FirstName = "Test Firstname",
+            LastName = "Test Lastname",
+            PhoneNumber = "+992923324252",
+            Country = "Test Country",
+            City = "Test City",
+            Address = "Test Address",
+            Gender = "Test Gender",
+            PostalCode = "Test Code",
+            DateOfBirth = new DateTime(2006, 06, 06)
+        };
+        var expectedResponse = "{error: Profile not found}";
+        var token = "Bearer test-token";
+
+        _mockHttpMessageHandler
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.NotFound,
+                Content = new StringContent(expectedResponse, Encoding.UTF8, "application/json")
+            });
+
+        var context = new DefaultHttpContext();
+        context.Request.Headers["Authorization"] = token;
+
+        var client = _httpClient;
+
+        // Act
+        var result = await CustomerEndpointsHandlers.UpdateCustomerProfile(
+            client,
+            context.Request, request);
+
+        // Assert
+        result.ShouldNotBeNull();
+        var contentResult = result.ShouldBeOfType<ContentHttpResult>();
+        contentResult.StatusCode.ShouldBe((int)HttpStatusCode.NotFound);
+        contentResult.ResponseContent.ShouldBe(expectedResponse);
+    }
+}
